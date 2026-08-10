d40318a9f77fbda89cd0c1f03626be6c4f4acafb
Scratchaker <105060449+Scratchaker@users.noreply.github.com>
2026-08-10 15:37:56 +0200

    Moved update instruction and removed theme var

--- 01-OMZconfig.sh
+++ 01-OMZconfig.sh
@@ -1,14 +1,13 @@
 # Path to your Oh My Zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
+# Disable automatic updates
+zstyle ':omz:update' mode disabled
+
 # Plugins
 ZSH_DISABLE_COMPFIX=true
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 source $ZSH/oh-my-zsh.sh
-
-# Theme
-ZSH_THEME="robbyrussell"
-zstyle ':omz:update' mode disabled  # disable automatic updates
 
 # Disable underline in zsh-syntax-highlighting
 ZSH_HIGHLIGHT_STYLES[path]=fg=cyan
