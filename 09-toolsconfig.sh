bbf39aeca99d8c424cd046ede4fe47946cd4ca75
Scratchaker <105060449+Scratchaker@users.noreply.github.com>
2026-08-28 16:12:06 +0200

    Add command-not-found support for multiple distros

--- 09-toolsconfig.sh
+++ 09-toolsconfig.sh
@@ -5,5 +5,9 @@ eval "$(fzf --zsh)"
 eval "$(zoxide init zsh --cmd cd)"
 
 # Prompt for installation when no packcage found
+# Debian/Ubuntu (apt)
 [[ -f /etc/zsh_command_not_found ]] && . /etc/zsh_command_not_found
-
+# Arch Linux (pacman)
+[[ -f /usr/share/doc/pkgfile/command-not-found.zsh ]] && . /usr/share/doc/pkgfile/command-not-found.zsh
+# Fedora/RHEL (dnf)
+[[ -f /usr/libexec/pk-command-not-found ]] && . /usr/libexec/pk-command-not-found
