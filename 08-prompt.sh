5fe764a0a4c12c57ba203c1edef6020c1f2b80c8
Scratchaker <105060449+Scratchaker@users.noreply.github.com>
2026-09-12 18:06:29 +0200

    Add powerlevel10k compatibility

--- 08-prompt.sh
+++ 08-prompt.sh
@@ -1,2 +1,5 @@
 # New, mimimal prompt
 PROMPT='%F{blue}%~%f %(?.%F{green}.%F{red})>%f '
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ -f $HOME/.p10k.zsh && -f $HOME/.powerlevel10k/powerlevel10k.zsh-theme ]] && source $HOME/.p10k.zsh && source $HOME/.powerlevel10k/powerlevel10k.zsh-theme || true
